40c3a57d3ec4d738672b6ccb79f8849c1db26894
Joel AZEMAR <joel.azemar@gmail.com>
2014-08-28 13:19:33 +0200

    ensure ignore_blank_line have default value

--- spec/csv2hash_spec.rb
+++ spec/csv2hash_spec.rb
@@ -1,3 +1,17 @@
 require 'spec_helper'
 
-describe Csv2hash
+describe Csv2hash do
+  let(:definition)  { double('definition', type: nil) }
+  let(:data_source) { nil }
+
+  subject do
+    Csv2hash::Main.new(definition, data_source, ignore_blank_line: false)
+  end
+
+  context '...' do
+    specify do
+      expect(subject.options.fetch(:ignore_blank_line)).to eql(false)
+    end
+  end
+
+end
